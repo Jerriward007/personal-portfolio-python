06f65a0d8b611585fb4cd63975c5614cec0005d4
Ogungbemi Jeremiah Kola <107107380+Jerriward007@users.noreply.github.com>
2025-07-04 18:56:09 +0100

    Update app.py

--- app.py
+++ app.py
@@ -18,4 +18,5 @@ def home():
     return render_template("index.html", projects=portfolio_projects)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+app.run(host="0.0.0.0", port=port)
